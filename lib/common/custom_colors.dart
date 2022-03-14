e36e21730732042446ae202d110dbad2bfcd3bc1
yasas <manukayasas99@gmail.com>
2022-03-14 19:34:46 +0530

    added common files

---
+++ lib/common/custom_colors.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+class CustomColors {
+  static const app_primary = Color(0xFF6E78F7);
+  static const app_bg_scaffold = Color(0xFFF5F5F5);
+  static const bg_grey = Color(0xFF94A5A6); //for message
+
+//  text
+  static const lightTextColor = Color(0xFFF6F6F6);
+  static const bodyTextColor = Color(0xFFFFFFFF);
+  static const blackLightTextColor = Color(0xFF333348);
+  static const blackLight2TextColor = Color(0xFF898A8F);
+  static const blackLight3TextColor = Color(0xFF3F4079);
+  static const hintTextColor = Color(0xFF76848B);
+  static const blackDark1TextColor = Color(0xFF313450);
+  static const blueTextColor = Color(0xFF7A3FE1);
+
+//  icon
+  static const iconNotSelected = Color(0xFFB0B3D0);
+  static const iconSelected = Color(0xFF3A58FC);
+  static const icon2 = Color(0xFFABC5EA);
+  static const icon3 = Color(0xFF6E78F7);
+  static const stoke = Color(0xFFE5D6FF);
+  static const starGreen = Color(0xFF27AE60);
+
+//  shadow
+  static const shadowColor = Color.fromRGBO(0, 0, 0, 0.25);
+
+//  divya
+  static const dBlue = Color(0xFF6E78F7);
+  static const dGrey = Color(0xFFE5E5E5);
+}
